53943f56257e24f9c8a5b6b7c84ec47145eb6def
Tina Eilertsen <tina.eilertsen@bouvet.no>
2026-02-03 16:35:53 +0100

    created new custom version of google analytics app since the old one we forked from is archived.

--- src/main/resources/site/processors/ga-report.js
+++ src/main/resources/site/processors/ga-report.js
@@ -1,53 +1,73 @@
-const contentLib = require('/lib/xp/content');
-const portalLib = require('/lib/xp/portal');
+const libs = {
+  portal: require('/lib/xp/portal'),
+  content: require('/lib/xp/content'),
+};
+
+const getDefaultScript = (measurementID, enableAnonymization) => {
+  const snippet = `window.dataLayer = window.dataLayer || []; \
+    function gtag(){dataLayer.push(arguments);} \
+    gtag('js', new Date()); \
+    gtag('config', '${measurementID}');
+    ${enableAnonymization ? `gtag('config','${measurementID}',{'anonymize_ip':true});` : ""}`;
+  return snippet;
+};
+
+const getConsentRequiredScript = (script, defaultDisable, measurementID) => {
+  const snippet = `var ga4Script = "${script}"; \
+    window.__RUN_ON_COOKIE_CONSENT__ = window.__RUN_ON_COOKIE_CONSENT__ || {}; \
+    window.__RUN_ON_COOKIE_CONSENT__["${defaultDisable}"] = function () { \
+      var s = document.createElement("script"); \
+      var gtagScript = document.createElement("script"); \
+      gtagScript.async = true; \
+      gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=${measurementID}"; \
+      s.id = "google-analytics-GA4-consent"; \
+      s.innerText = ga4Script; \
+      document.getElementsByTagName("head")[0].appendChild(gtagScript); \
+      document.getElementsByTagName("head")[0].appendChild(s); \
+    }`;
+  return snippet;
+};
 
 //TODO check if tracking is enabled or not
 
 exports.responseProcessor = function (req, res) {
-    let contentId = req.params.contentId;
-    if (!contentId) {
-        const fetchContent = portalLib.getContent();
-        if (fetchContent) {
-            contentId = fetchContent._id;
-        } else {
-            return res;
-        }
-    }
+  if (req.mode !== 'live') {
+    return res;
+  }
 
-    const siteConfig = contentLib.getSiteConfig({
-        key: contentId,
-        applicationKey: app.name
-    });
+  const site = libs.portal.getSite().config;
+  const defaultDisable = app.name.replace(/\./g, "-") + "_disabled";
 
-    const measurementID = siteConfig.measurementId || '';
-    const enableTracking = siteConfig.enableTracking || false;
-    const enableAnonymization = siteConfig.enableAnonymization || false;
+  if (site && site._path) {
+    const siteConfig = libs.portal.getSiteConfig() || {};
+
+    const measurementID = siteConfig?.measurementId || '';
+    const enableTracking = siteConfig?.enableTracking || false;
+    const enableAnonymization = siteConfig?.enableAnonymization || false;
 
     if (!measurementID || !enableTracking) {
-        return res;
+      return res;
     }
 
-    if (req.mode !== 'live') {
-        return res;
-    }
+    let script = getDefaultScript(measurementID, enableAnonymization);
+    script = getConsentRequiredScript(script, defaultDisable, measurementID);
 
-    let snippet = `<script async src="https://www.googletagmanager.com/gtag/js?id=${measurementID}"></script>
-        <script>
-            window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config','${measurementID}');`
+    const snippet = `<!-- Global site tag (gtag.js) - Google Analytics --> \
+        <script> \
+        ${script} \
+        </script> \
+        <!-- End Global site tag (gtag.js) - Google Analytics -->`;
 
-    if (enableAnonymization) {
-        snippet += `gtag('config','${measurementID}',{'anonymize_ip':true});`;
-    }
-    snippet += "</script>";
 
     const headEnd = res.pageContributions.headEnd;
     if (!headEnd) {
-        res.pageContributions.headEnd = [];
+      res.pageContributions.headEnd = [];
     }
     else if (typeof (headEnd) == 'string') {
-        res.pageContributions.headEnd = [headEnd];
+      res.pageContributions.headEnd = [headEnd];
     }
 
     res.pageContributions.headEnd.push(snippet);
-    return res;
+  }
+  return res;
 };
