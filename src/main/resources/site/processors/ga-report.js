38e930a40bec2f3ed821d4e5613c39a7dd775b95
Tina Eilertsen <tina.eilertsen@bouvet.no>
2026-02-05 08:52:34 +0100

    Increase version number - 4.1.0

--- src/main/resources/site/processors/ga-report.js
+++ src/main/resources/site/processors/ga-report.js
@@ -6,19 +6,19 @@ const getDefaultScript = (measurementID, enableAnonymization) => {
   const snippet = `window.dataLayer = window.dataLayer || []; \
     function gtag(){dataLayer.push(arguments);} \
     gtag('js', new Date()); \
-    gtag('config', '${measurementID}');
-    ${enableAnonymization ? `gtag('config','${measurementID}',{'anonymize_ip':true});` : ""}`;
+    gtag('config', '${measurementID}'${enableAnonymization ? `, {'anonymize_ip':true}` : ''}); \
+    `;
   return snippet;
 };
 
 const getConsentRequiredScript = (script, defaultDisable, measurementID) => {
   const snippet = `var ga4Script = "${script}"; \
     window.__RUN_ON_COOKIE_CONSENT__ = window.__RUN_ON_COOKIE_CONSENT__ || {}; \
-    window.__RUN_ON_COOKIE_CONSENT__["${defaultDisable}"] = function () { \
+    window.__RUN_ON_COOKIE_CONSENT__['${defaultDisable}'] = function () { \
       var s = document.createElement("script"); \
       var gtagScript = document.createElement("script"); \
       gtagScript.async = true; \
-      gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=${measurementID}"; \
+      gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=${measurementID}'; \
       s.id = "google-analytics-GA4-consent"; \
       s.innerText = ga4Script; \
       document.getElementsByTagName("head")[0].appendChild(gtagScript); \
@@ -40,9 +40,9 @@ exports.responseProcessor = function (req, res) {
   if (site && site._path) {
     const siteConfig = libs.portal.getSiteConfig() || {};
 
-    const measurementID = siteConfig?.measurementId || '';
-    const enableTracking = siteConfig?.enableTracking || false;
-    const enableAnonymization = siteConfig?.enableAnonymization || false;
+    const measurementID = siteConfig.measurementId || '';
+    const enableTracking = siteConfig.enableTracking || false;
+    const enableAnonymization = siteConfig.enableAnonymization || false;
 
     if (!measurementID || !enableTracking) {
       return res;
@@ -51,11 +51,11 @@ exports.responseProcessor = function (req, res) {
     let script = getDefaultScript(measurementID, enableAnonymization);
     script = getConsentRequiredScript(script, defaultDisable, measurementID);
 
-    const snippet = `<!-- Global site tag (gtag.js) - Google Analytics --> \
+    const snippet = `<!-- Google Analytics --> \
         <script> \
         ${script} \
         </script> \
-        <!-- End Global site tag (gtag.js) - Google Analytics -->`;
+        <!-- End Google Analytics -->`;
 
 
     const headEnd = res.pageContributions.headEnd;
